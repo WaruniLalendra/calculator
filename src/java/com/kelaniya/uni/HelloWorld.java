534f8acabafaa8b0781dbfa744ce340c657efff2
waruniLalendra <warunilalendra@gmail.com>
2021-08-08 01:55:07 -0700

    HelloWorld updated

--- src/java/com/kelaniya/uni/HelloWorld.java
+++ src/java/com/kelaniya/uni/HelloWorld.java
@@ -2,6 +2,13 @@ package com.kelaniya.uni;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("hello world");
+        //System.out.println("hello world");
+
+        GetData data = new GetData();
+        OperationHandler op = new OperationHandler();
+
+        data.getData();
+        op.selectOperation();
+
     }
 }
